b4eb6755ec772f4d1815fb1e8c3300a88c7c8c3d
Jorge Saba <pve_jjorge@uolinc.com>
2021-08-17 19:46:36 -0300

    adding POJOS. Closes #2

---
+++ src/main/java/com/example/sfgpetclinic/model/Pet.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinic.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Pet {
+
+    private LocalDate birthDate;
+    private PetType petType;
+    private Owner owner;
+}
---
+++ src/main/java/com/example/sfgpetclinic/model/PetType.java
@@ -0,0 +1,8 @@
+package com.example.sfgpetclinic.model;
+
+import lombok.Data;
+
+@Data
+public class PetType {
+    private String name;
+}
